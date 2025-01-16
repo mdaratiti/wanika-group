78dc3dc2d4510d7396c4774d2c01c6840e8ac1b7
Watson <watsonmatsika@gmail.com>
2025-01-16 14:27:20 +0200

    deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-};
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    // output: 'export',
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    images: {
+        unoptimized: true,
+    },
+    experimental: {
+        webpackBuildWorker: true,
+        parallelServerBuildTraces: true,
+        parallelServerCompiles: true,
+    },
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
+}
 
 export default nextConfig;
